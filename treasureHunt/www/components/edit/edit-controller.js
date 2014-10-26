6d620e4f30ff566ead9c5b52b04a17bf4676f2ab
Bjorn Holdt <bjornholdt@Bjorns-MacBook-Pro-2.local>
2014-10-26 20:58:20 +1000

    Delete clues from treasure hunt

--- treasureHunt/www/components/edit/edit-controller.js
+++ treasureHunt/www/components/edit/edit-controller.js
@@ -1,4 +1,13 @@
-app.controller('EditController', function($scope, treasureHuntRepository){
+app.controller('EditController', function($scope, $state, treasureHuntRepository){
     var treasureHunt = treasureHuntRepository.getTreasureHunt();
     $scope.treasureHunt = treasureHunt;
+
+    $scope.deleteClue = function(index){
+      treasureHunt.clues.splice(index, 1);
+    }
+
+    $scope.save = function(){
+      treasureHuntRepository.saveTreasureHunt(treasureHunt);
+      $state.go('#/details');
+    }
 });
