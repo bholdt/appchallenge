3e3cf569232b9377ce1d4a29279994567449bc55
Bjorn Holdt <bm.holdt@gmail.com>
2014-10-27 15:51:09 +1000

    Help shown when playing

--- treasureHunt/www/components/play/play-controller.js
+++ treasureHunt/www/components/play/play-controller.js
@@ -1,22 +1,53 @@
-app.controller('PlayController', function($scope, treasureHuntContext, $cordovaDeviceOrientation, distanceService, playDistanceColourService){
-  var treasureHunt = treasureHuntContext.get();
-  var currentClueIndex = 0;
-  var clue = {}
-  clue.current = treasureHunt.clues[currentClueIndex];
-  var currentPosition = {};
-  var intervalId;
+app.controller('PlayController', function($scope, $timeout, $ionicModal, treasureHuntContext){
 
-  function onNewPosition(position) {
-    if(position.coords && $scope.clue.current.position){
-      currentPosition = position;
-      updateClue();
-    }
-    $scope.$apply();
+  var treasureHunt = treasureHuntContext.get();
+  var clue = {};
+  var currentClueIndex;
+
+  function init(){
+    currentClueIndex = 0;
+    clue = treasureHunt.clues[currentClueIndex];
+    $scope.clue = clue;
+
+    $ionicModal.fromTemplateUrl('help.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.helpModal = modal;
+    });
   }
 
-  $scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
-    clearInterval(intervalId);
+  $scope.showHelp = function() {
+    var helps = {};
+    $scope.helps = helps;
+    $scope.isHelp = true;
+    $scope.helps.distanceTo = 300;
+    $scope.helps.bearingTo = 20;
+    $scope.helpModal.show();
+  };
+
+  $scope.cancelHelp = function() {
+    $scope.helpModal.hide();
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
   });
+
+  function updateClue(){
+    clue = treasureHunt.clues[currentClueIndex];
+    $scope.clue = clue;
+  }
+
+  var getCurrentPosition = function(){
+    var options = { timeout: 4000, enableHighAccuracy: true };
+    navigator.geolocation.getCurrentPosition(onNewPosition, function(error){ alert('Something went wrong: ' + error.message); }, options);
+  }
+
+  function onNewPosition(position){
+    console.log(position);
+  }
 
   $scope.nextClue = function(){
     if(currentClueIndex < treasureHunt.clues.length - 1){
@@ -30,60 +61,6 @@ app.controller('PlayController', function($scope, treasureHuntContext, $cordovaD
       currentClueIndex--;
     }
     updateClue();
-  }
-
-  function updateClue(){
-    $scope.clue.current = treasureHunt.clues[currentClueIndex];
-    $scope.clue.current.distanceTo = 0;
-    //$scope.clue.current.direction = ($scope.clue.current.direction || 0) - 40;
-    if(currentPosition.coords){
-      $scope.clue.current.distanceTo = distanceService.calculateDistance(currentPosition.coords,$scope.clue.current.position);
-    }
-    $scope.distanceClass = playDistanceColourService.getColour($scope.clue.current.distanceTo);
-  }
-
-  var getCurrentPosition = function(){
-    var options = { timeout: 4000, enableHighAccuracy: true };
-    navigator.geolocation.getCurrentPosition(onNewPosition, function(){}, options);
-  }
-
-  var init = function(){
-    intervalId = setInterval(getCurrentPosition, 5000);
-
-    $scope.treasureHunt = treasureHunt;
-    $scope.clue = clue;
-    $scope.distanceClass = 's-distance';
-    try {
-    var options = { frequency: 1000 }; // Update every 1 second
-
-   var watch = $cordovaDeviceOrientation.watchHeading(options);
-
-   watch.promise.then(function(result) { /* unused */ },
-     function(err) {
-       // An error occurred
-       alert(err);
-     }, function(position) {
-       if($scope.clue.current) {
-         if(currentPosition && currentPosition.coords && $scope.clue.current.position){
-         var current = new LatLon(currentPosition.coords.latitude, currentPosition.coords.longitude);
-         $scope.currentPosition = currentPosition.coords;
-         var cluePosition = $scope.clue.current.position;
-         $scope.destinationPosition = $scope.clue.current.position;
-         var clue = new LatLon(cluePosition.latitude, cluePosition.longitude);
-         var bearing = Math.round(current.bearingTo(clue));
-         $scope.bearing = bearing;
-         var diff = bearing - Math.round(position.magneticHeading);
-         $scope.distance = $scope.clue.current.distanceTo.toFixed(2);
-         $scope.finalBearing= diff;
-         $scope.clue.current.direction = diff;
-         $scope.image= $scope.clue.current.image;
-         $scope.$apply();
-       }
-       }
-     });
-   }
-   catch(e) { }
-
   }
 
   init();
